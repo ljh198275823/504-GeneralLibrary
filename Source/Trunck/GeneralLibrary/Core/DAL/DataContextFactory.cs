55ab7c4fbae43e7749c842592817936bbc59147d
bruce <ljh198275823@163.com>
2014-04-28 21:01:06 +0800

    1.增加数据库类型判断

--- Source/Trunck/GeneralLibrary/Core/DAL/DataContextFactory.cs
+++ Source/Trunck/GeneralLibrary/Core/DAL/DataContextFactory.cs
@@ -1,6 +1,9 @@
 ﻿using System.IO;
 using System.Data.Linq.Mapping;
 using System.Data.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SQLite;
 
 namespace LJH.GeneralLibrary.Core.DAL
 {
@@ -14,9 +17,57 @@ namespace LJH.GeneralLibrary.Core.DAL
         public static DataContext CreateDataContext(string connStr, MappingSource ms, bool Log = false)
         {
             System.Diagnostics.Debug.Assert(!string.IsNullOrEmpty(connStr), "没有找到有效的数据库连接!");
-            DataContext dc = new DataContext(connStr, ms);
+            IDbConnection connection = null;
+            string sqlType = GetSQLType(connStr);
+            if (sqlType == "SQLITE")
+            {
+                connection = new SQLiteConnection(GetConnectString(connStr));
+            }
+            else if (sqlType == "MSSQL")
+            {
+                connection = new SqlConnection(GetConnectString(connStr));
+            }
+            else //如果没有标明数据库类型，默认采用mssql数据库
+            {
+                connection = new SqlConnection(connStr);
+            }
+            DataContext dc = new DataContext(connection, ms);
             if (Log) dc.Log = System.Console.Out;
             return dc;
+        }
+
+        /// <summary>
+        /// 从数据库连接资源uri中获取数据库类型
+        /// </summary>
+        /// <returns></returns>
+        private static string GetSQLType(string connStr)
+        {
+            if (!string.IsNullOrEmpty(connStr))
+            {
+                int p = connStr.IndexOf(':');
+                if (p > 0)
+                {
+                    return connStr.Substring(0, p);
+                }
+            }
+            return string.Empty;
+        }
+
+        /// <summary>
+        ///  从数据库连接资源uri获取数据库连接字符串
+        /// </summary>
+        /// <returns></returns>
+        private static string GetConnectString(string connStr)
+        {
+            if (!string.IsNullOrEmpty(connStr))
+            {
+                int p = connStr.IndexOf(':');
+                if (p > 0)
+                {
+                    return connStr.Substring(p + 1);
+                }
+            }
+            return string.Empty;
         }
     }
 }
