236e6d02884b687c7c3e03c370cf66e9dcb575e7
ljh198275823 <ljh198275823@163.com>
2015-01-05 18:08:07 +0800

    1

--- Source/Trunck/GeneralLibrary/WinformControl/PercentageProgressBar.cs
+++ Source/Trunck/GeneralLibrary/WinformControl/PercentageProgressBar.cs
@@ -83,69 +83,21 @@ namespace LJH.GeneralLibrary.WinformControl
             base.WndProc(ref m);
             if (m.Msg == 0xf || m.Msg == 0x133)
             {
-                //拦截系统消息，获得当前控件进程以便重绘。  
-
-                //一些控件（如TextBox、Button等）是由系统进程绘制，重载OnPaint方法将不起作用.  
-
-                //所有这里并没有使用重载OnPaint方法绘制TextBox边框。  
-
-                //  
-
-                //MSDN:重写   OnPaint   将禁止修改所有控件的外观。  
-
-                //那些由   Windows   完成其所有绘图的控件（例如   Textbox）从不调用它们的   OnPaint   方法，  
-
-                //因此将永远不会使用自定义代码。请参见您要修改的特定控件的文档，  
-
-                //查看   OnPaint   方法是否可用。如果某个控件未将   OnPaint   作为成员方法列出，  
-
-                //则您无法通过重写此方法改变其外观。  
-
-                //  
-
-                //MSDN:要了解可用的   Message.Msg、Message.LParam   和   Message.WParam   值，  
-
-                //请参考位于   MSDN   Library   中的   Platform   SDK   文档参考。可在   Platform   SDK（“Core   SDK”一节）  
-
-                //下载中包含的   windows.h   头文件中找到实际常数值，该文件也可在   MSDN   上找到。  
-
                 IntPtr hDC = GetWindowDC(m.HWnd);
-
-                if (hDC.ToInt32() == 0)
+                if (hDC.ToInt32() == 0) return;
+                using (System.Drawing.Graphics g = Graphics.FromHdc(hDC))
                 {
-
-                    return;
-
+                    SolidBrush brush = new SolidBrush(_TextColor);
+                    string s = this.Maximum == 0 ? "100%" : string.Format("{0}%", this.Value * 100 / this.Maximum);
+                    SizeF size = g.MeasureString(s, _TextFont);
+                    float x = (this.Width - size.Width) / 2;
+                    float y = (this.Height - size.Height) / 2;
+                    g.DrawString(s, _TextFont, brush, x, y);
+                    m.Result = IntPtr.Zero;
+                    ReleaseDC(m.HWnd, hDC);
                 }
-
-                //base.OnPaint(e);
-
-                System.Drawing.Graphics g = Graphics.FromHdc(hDC);
-
-                SolidBrush brush = new SolidBrush(_TextColor);
-
-                string s = this.Maximum == 0 ? "100%" : string.Format("{0}%", this.Value * 100 / this.Maximum);
-
-                SizeF size = g.MeasureString(s, _TextFont);
-
-                float x = (this.Width - size.Width) / 2;
-
-                float y = (this.Height - size.Height) / 2;
-
-                g.DrawString(s, _TextFont, brush, x, y);
-
-                //返回结果  
-
-                m.Result = IntPtr.Zero;
-
-                //释放  
-
-                ReleaseDC(m.HWnd, hDC);
-
             }
-
         }
-
         #endregion
     }
 }
---
+++ Source/Trunck/ProcessBarTest/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace ProcessBarTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.percentageProgressBar1 = new LJH.GeneralLibrary.WinformControl.PercentageProgressBar(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // percentageProgressBar1
+            // 
+            this.percentageProgressBar1.Location = new System.Drawing.Point(62, 50);
+            this.percentageProgressBar1.Name = "percentageProgressBar1";
+            this.percentageProgressBar1.Size = new System.Drawing.Size(535, 72);
+            this.percentageProgressBar1.TabIndex = 0;
+            this.percentageProgressBar1.TextColor = System.Drawing.Color.Black;
+            this.percentageProgressBar1.TextFont = new System.Drawing.Font("宋体", 9F);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(453, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 234);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.percentageProgressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private LJH.GeneralLibrary.WinformControl.PercentageProgressBar percentageProgressBar1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
