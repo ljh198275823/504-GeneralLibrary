d705ea965ddee9bd18d044f91203dd2a5c1ed96c
ljh198275823 <ljh198275823@163.com>
2015-05-14 15:17:10 +0800

    1

---
+++ Barcode/_2DTest/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace _2DTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.qrCodeGraphicControl1 = new Gma.QrCodeNet.Encoding.Windows.Forms.QrCodeGraphicControl();
+            this.qrCodeImgControl1 = new Gma.QrCodeNet.Encoding.Windows.Forms.QrCodeImgControl();
+            ((System.ComponentModel.ISupportInitialize)(this.qrCodeImgControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(12, 36);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(464, 69);
+            this.txtContent.TabIndex = 1;
+            this.txtContent.Text = "www.huaxiahuihai.com.cn";
+            this.txtContent.TextChanged += new System.EventHandler(this.txtContent_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "二维码内容";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(49, 385);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(107, 12);
+            this.lblInfo.TabIndex = 8;
+            this.lblInfo.Text = "Width=0  Height=0";
+            // 
+            // qrCodeGraphicControl1
+            // 
+            this.qrCodeGraphicControl1.ErrorCorrectLevel = Gma.QrCodeNet.Encoding.ErrorCorrectionLevel.M;
+            this.qrCodeGraphicControl1.Location = new System.Drawing.Point(15, 124);
+            this.qrCodeGraphicControl1.Name = "qrCodeGraphicControl1";
+            this.qrCodeGraphicControl1.QuietZoneModule = Gma.QrCodeNet.Encoding.Windows.Render.QuietZoneModules.Two;
+            this.qrCodeGraphicControl1.Size = new System.Drawing.Size(299, 215);
+            this.qrCodeGraphicControl1.TabIndex = 9;
+            this.qrCodeGraphicControl1.Text = "qrCodeGraphicControl1";
+            // 
+            // qrCodeImgControl1
+            // 
+            this.qrCodeImgControl1.ErrorCorrectLevel = Gma.QrCodeNet.Encoding.ErrorCorrectionLevel.M;
+            this.qrCodeImgControl1.Image = ((System.Drawing.Image)(resources.GetObject("qrCodeImgControl1.Image")));
+            this.qrCodeImgControl1.Location = new System.Drawing.Point(352, 124);
+            this.qrCodeImgControl1.Name = "qrCodeImgControl1";
+            this.qrCodeImgControl1.QuietZoneModule = Gma.QrCodeNet.Encoding.Windows.Render.QuietZoneModules.Two;
+            this.qrCodeImgControl1.Size = new System.Drawing.Size(165, 164);
+            this.qrCodeImgControl1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.qrCodeImgControl1.TabIndex = 10;
+            this.qrCodeImgControl1.TabStop = false;
+            this.qrCodeImgControl1.Text = "qrCodeImgControl1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 432);
+            this.Controls.Add(this.qrCodeImgControl1);
+            this.Controls.Add(this.qrCodeGraphicControl1);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtContent);
+            this.Name = "Form1";
+            this.Text = "二维码测试";
+            ((System.ComponentModel.ISupportInitialize)(this.qrCodeImgControl1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblInfo;
+        private Gma.QrCodeNet.Encoding.Windows.Forms.QrCodeGraphicControl qrCodeGraphicControl1;
+        private Gma.QrCodeNet.Encoding.Windows.Forms.QrCodeImgControl qrCodeImgControl1;
+    }
+}
+
---
+++ Barcode/_2DTest/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Gma.QrCodeNet.Encoding;
+using Gma.QrCodeNet.Encoding.Windows.Render;
+
+namespace _2DTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            qrCodeGraphicControl1.Text = txtContent.Text;
+            qrCodeImgControl1.Text = txtContent.Text;
+        }
+
+        private void ContrastCal()
+        {
+            //SolidBrush darkmoduleBrush = qrCodeImgControl1.DarkBrush as SolidBrush;
+            //SolidBrush lightmoduleBrush = qrCodeImgControl1.LightBrush as SolidBrush;
+            //Color darkmodule = darkmoduleBrush == null ? _darkModule : darkmoduleBrush.Color;
+            //Color lightmodule = lightmoduleBrush == null ? _lightModule : lightmoduleBrush.Color;
+
+            //Contrast ctrast = ColorContrast.GetContrast(new FormColor(lightmodule), new FormColor(darkmodule));
+
+            //label1.Text = ctrast.Ratio.ToString();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtContent_TextChanged(object sender, EventArgs e)
+        {
+            qrCodeGraphicControl1.Text = txtContent.Text;
+            qrCodeImgControl1.Text = txtContent.Text;
+        }
+    }
+}
