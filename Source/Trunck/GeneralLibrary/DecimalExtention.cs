a915f0286506a7862f87ddde361e38aa8b497fc1
ljh198275823 <ljh198275823@163.com>
2015-08-27 17:12:55 +0800

    1

--- Source/Trunck/GeneralLibrary/DecimalExtention.cs
+++ Source/Trunck/GeneralLibrary/DecimalExtention.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace LJH.GeneralLibrary
 {
-    public static class DecimalHelper
+    public static class DecimalExtention
     {
         /// <summary>
         /// 去掉实数小数点后不必要的0,比如12.500 会变成12.5,12.00会变成12等
