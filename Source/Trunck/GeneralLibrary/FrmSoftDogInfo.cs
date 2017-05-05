3151f347a8d943d41a8f635025f694dc3c7ac9f0
bruce <233955501@qq.com>
2017-05-05 12:04:37 +0800

    1。增加两个窗体

---
+++ Source/Trunck/GeneralLibrary/FrmSoftDogInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LJH.GeneralLibrary
+{
+    public partial class FrmSoftDogInfo : Form
+    {
+        public FrmSoftDogInfo()
+        {
+            InitializeComponent();
+        }
+
+        public LJH.GeneralLibrary.SoftDog.SoftDogInfo SoftDog { get; set; }
+
+        private void FrmSoftDogInfo_Load(object sender, EventArgs e)
+        {
+            if (SoftDog != null)
+            {
+                lblProjectNum.Text = SoftDog.ProjectNo.ToString();
+                lblStartDate.Text = SoftDog.StartDate.ToShortDateString();
+                lblExpireDate.Text = SoftDog.ExpiredDate.ToShortDateString();
+            }
+        }
+
+        private void FrmSoftDogInfo_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Source/Trunck/GeneralLibrary/FrmSoftDogInfo.designer.cs
@@ -0,0 +1,138 @@
+﻿namespace LJH.GeneralLibrary
+{
+    partial class FrmSoftDogInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblProjectNum = new System.Windows.Forms.Label();
+            this.lblStartDate = new System.Windows.Forms.Label();
+            this.lblExpireDate = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "项目编号:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "开始日期:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "有效期至:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblProjectNum
+            // 
+            this.lblProjectNum.AutoSize = true;
+            this.lblProjectNum.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblProjectNum.Location = new System.Drawing.Point(91, 46);
+            this.lblProjectNum.Name = "lblProjectNum";
+            this.lblProjectNum.Size = new System.Drawing.Size(82, 14);
+            this.lblProjectNum.TabIndex = 3;
+            this.lblProjectNum.Text = "没有加密狗";
+            this.lblProjectNum.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblStartDate
+            // 
+            this.lblStartDate.AutoSize = true;
+            this.lblStartDate.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblStartDate.Location = new System.Drawing.Point(91, 87);
+            this.lblStartDate.Name = "lblStartDate";
+            this.lblStartDate.Size = new System.Drawing.Size(82, 14);
+            this.lblStartDate.TabIndex = 4;
+            this.lblStartDate.Text = "没有加密狗";
+            this.lblStartDate.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblExpireDate
+            // 
+            this.lblExpireDate.AutoSize = true;
+            this.lblExpireDate.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblExpireDate.Location = new System.Drawing.Point(91, 125);
+            this.lblExpireDate.Name = "lblExpireDate";
+            this.lblExpireDate.Size = new System.Drawing.Size(82, 14);
+            this.lblExpireDate.TabIndex = 5;
+            this.lblExpireDate.Text = "没有加密狗";
+            this.lblExpireDate.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // FrmSoftDogInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::LJH.GeneralLibrary.Properties.Resources.img15;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(324, 179);
+            this.Controls.Add(this.lblExpireDate);
+            this.Controls.Add(this.lblStartDate);
+            this.Controls.Add(this.lblProjectNum);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmSoftDogInfo";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "软件授权信息";
+            this.Load += new System.EventHandler(this.FrmSoftDogInfo_Load);
+            this.Click += new System.EventHandler(this.FrmSoftDogInfo_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblProjectNum;
+        private System.Windows.Forms.Label lblStartDate;
+        private System.Windows.Forms.Label lblExpireDate;
+    }
+}
