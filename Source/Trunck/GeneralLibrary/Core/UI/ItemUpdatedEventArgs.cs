b9ea2315156f061a5a9200f03ab37209aae8be83
bruce <233955501@qq.com>
2018-09-10 12:13:23 +0800

    1

--- Source/Trunck/GeneralLibrary/Core/UI/ItemUpdatedEventArgs.cs
+++ Source/Trunck/GeneralLibrary/Core/UI/ItemUpdatedEventArgs.cs
@@ -8,12 +8,27 @@ namespace LJH.GeneralLibrary.Core.UI
     public class ItemUpdatedEventArgs : EventArgs
     {
         public ItemUpdatedEventArgs()
-        { }
+        {
+        }
 
         public ItemUpdatedEventArgs(object updatedItem)
         {
             this.UpdatedItem = updatedItem;
         }
+
+        public ItemUpdatedEventArgs(object updatedItem, object preItem)
+        {
+            UpdatedItem = updatedItem;
+            PreUpdatingItem = preItem;
+        }
+
+        /// <summary>
+        /// 获取或设置更新后的对象
+        /// </summary>
         public object UpdatedItem { get; set; }
+        /// <summary>
+        /// 获取或设置更新之前的对象
+        /// </summary>
+        public object PreUpdatingItem { get; set; }
     }
 }
