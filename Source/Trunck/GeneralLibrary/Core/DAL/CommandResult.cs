4adf8a4310da361d6440929430820d33a3e1e353
李建华 <ljh198275823@163.com>
2018-11-06 11:12:52 +0800

    1

--- Source/Trunck/GeneralLibrary/Core/DAL/CommandResult.cs
+++ Source/Trunck/GeneralLibrary/Core/DAL/CommandResult.cs
@@ -17,8 +17,8 @@ namespace LJH.GeneralLibrary.Core.DAL
 
         public CommandResult(ResultCode code, string msg)
         {
-            this.Result  = code;
-            this.Message  = msg;
+            this.Result = code;
+            this.Message = msg;
         }
         #endregion
 
@@ -26,11 +26,30 @@ namespace LJH.GeneralLibrary.Core.DAL
         /// <summary>
         ///获取或设置执行结果
         /// </summary>
-        public ResultCode Result{get;set;}
+        public ResultCode Result { get; set; }
         /// <summary>
         /// 获取或设置执行结果的文字描述
         /// </summary>
-        public string Message{get;set; }
+        public string Message { get; set; }
+        #endregion
+    }
+
+
+    /// <summary>
+    /// 表示数据操作返回值
+    /// </summary>
+    public class CommandResult<T> : CommandResult where T : class
+    {
+        #region 构造函数
+        public CommandResult(ResultCode code, string msg, T t)
+            : base(code, msg)
+        {
+            Value = t;
+        }
+        #endregion
+
+        #region 公共属性
+        public T Value { get; set; }
         #endregion
     }
 }
---
+++ Source/Trunck/GeneralLibrary/DateTimeRange.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace LJH.GeneralLibrary
+{
+    /// <summary>
+    /// 表示一个时间段
+    /// </summary>
+    [Serializable]
+    public class DateTimeRange
+    {
+        /// <summary>
+        /// 开始时间(如果不指定为日期的最小值)
+        /// </summary>
+        public DateTime Begin { get; set; }
+
+        /// <summary>
+        /// 结束时间(如果不指定为日期的最大值)
+        /// </summary>
+        public DateTime End { get; set; }
+
+        public DateTimeRange()
+        {
+            Begin = DateTime.MinValue ;
+            End = DateTime.MaxValue;
+        }
+
+        public DateTimeRange(DateTime bengin, DateTime dtend)
+        {
+            Begin  = bengin;
+            End = dtend;
+        }
+    }
+}
