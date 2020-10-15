99bfd8c5f27bc93662ec9e964f8e1496d444e29b
李建华 <ljh198275823@163.com>
2020-10-15 18:26:19 +0800

    1

---
+++ Source/Trunck/GeneralLibrary/Core/DAL/ImportOption.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LJH.GeneralLibrary.Core.DAL
+{
+    public enum ImportOption
+    {
+        /// <summary>
+        /// 忽略
+        /// </summary>
+        Ignore = 0,
+        /// <summary>
+        /// 覆盖
+        /// </summary>
+        Override = 1,
+        /// <summary>
+        /// 追加
+        /// </summary>
+        Append = 2,
+    }
+}
---
+++ Source/Trunck/GeneralLibrary/Core/DAL/UpdateItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LJH.GeneralLibrary.Core.DAL
+{
+    /// <summary>
+    /// 表示一个修改项
+    /// </summary>
+    public class UpdateItem<TID>
+    {
+        public TID ID { get; set; }
+        /// <summary>
+        /// 获取或设置修改项的ID, 一般者了实体类的属性名称
+        /// </summary>
+        public string Key { get; set; }
+        /// <summary>
+        /// 获取或设置修改后的新值
+        /// </summary>
+        public string Value { get; set; }
+        /// <summary>
+        /// 获取或设置修改前的值 
+        /// </summary>
+        public string OriginalValue { get; set; }
+    }
+}
